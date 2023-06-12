2f4b064b04f145b9c17c2513837e300d063217df
MaggieWang0614 <maggiewang@asu.edu>
2023-06-12 11:44:11 -0700

    update treatment effect definition

--- R/predict.longBet.R
+++ R/predict.longBet.R
@@ -87,7 +87,9 @@ predict.longBet <- function(model, x, z, sigma = NULL, lambda = NULL, ...) {
     seq <- (num_burnin+1):num_sweeps
     for (i in seq) {
         obj$muhats[,, i - num_burnin] = matrix(obj_mu$preds[,i], n, p) * (model$a_draws[i]) + model$meany +  matrix(obj_tau$preds[,i], n, p) *  model$b_draws[i,1] * beta_preds[,,i]
-        obj$tauhats[,, i - num_burnin] = matrix(obj_tau$preds[,i], n, p) * (model$b_draws[i,2] - model$b_draws[i,1]) * beta_preds[,,i]
+        # obj$tauhats[,, i - num_burnin] = matrix(obj_tau$preds[,i], n, p) * (model$b_draws[i,2] - model$b_draws[i,1]) * beta_preds[,,i]
+        obj$tauhats[,, i - num_burnin] = matrix(obj_tau$preds[,i], n, p) * (model$b_draws[i,2] * beta_preds[,,i] - model$b_draws[i,1] * model$beta_draws[1, i]) # * beta_preds[,,i]
+        # TODO: change tauhat to b1 * beta_s * tau_s - b0 * beta_0 * tau_0 when tau can split on post-treatment time
     }
     obj$beta_preds <- beta_preds
     # obj$beta_draws = beta

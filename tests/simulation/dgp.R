4c05c7ad4a889ee9ff7e17815a61c442bba2642b
MaggieWang0614 <maggiewang@asu.edu>
2023-11-28 14:12:12 -0800

    add test code for extrapolation

--- tests/simulation/dgp.R
+++ tests/simulation/dgp.R
@@ -16,7 +16,9 @@ require(forecast)
 # y0: potential outcomes 
 # tau: treatment effect
 
-dgp <- function(n, t0 = 6, t1 = 12, pr_type = "non-parallel", trt_type = "heterogeneous"){
+dgp <- function(n, t0 = 6, t1 = 12, t2 = NULL, pr_type = "non-parallel", trt_type = "heterogeneous"){
+  # t2: Extended period for prediction
+  
   # screening
   if (t0 > t1){
     stop("Treatment start time (t0) can not be greater than t1")
@@ -26,6 +28,10 @@ dgp <- function(n, t0 = 6, t1 = 12, pr_type = "non-parallel", trt_type = "hetero
   }
   if(!(trt_type %in% c("homogeneous", "heterogeneous"))){
     stop("trt_type should be either homogeneous or heterogeneous")
+  }
+  
+  if (is.null(t2)) {
+    t2 <- t1
   }
   
   results <- list()
@@ -39,11 +45,11 @@ dgp <- function(n, t0 = 6, t1 = 12, pr_type = "non-parallel", trt_type = "hetero
   results$x <- x
   
   # generate prognostic effect
-  f_t <- arima.sim(model = list(order = c(1, 0, 1), ar = 0.7, ma = -0.4), n = t1) + 1
+  f_t <- arima.sim(model = list(order = c(1, 0, 1), ar = 0.7, ma = -0.4), n = t2) + 1
   g <- c(2, -1)
   if(pr_type == "parallel"){
     gamma_x <-  g[x[, 4] + 1] + 1 * x[, 1] * abs(x[, 3] - 1)
-    y0 <- t( matrix(rep(f_t, n), t1, n) ) + matrix(rep(gamma_x, t1), n, t1)
+    y0 <- t( matrix(rep(f_t, n), t2, n) ) + matrix(rep(gamma_x, t2), n, t2)
   } else if (pr_type == "non-parallel"){
     gamma_x <-  g[x[, 4] + 1] + 1 * x[, 1] * abs(x[, 3] - 1)
     y0 <- outer(gamma_x, f_t, "*")
@@ -51,7 +57,7 @@ dgp <- function(n, t0 = 6, t1 = 12, pr_type = "non-parallel", trt_type = "hetero
   results$y0 <- y0
   
   # generate treatment effect 
-  s <- 1:(t1 - t0 + 1) 
+  s <- 1:(t2 - t0 + 1) 
   h_s <- s * exp(-s)
   if(trt_type == "homogeneous"){
     nu_x <- rep(2, n)
@@ -62,11 +68,14 @@ dgp <- function(n, t0 = 6, t1 = 12, pr_type = "non-parallel", trt_type = "hetero
   
   # compute propensity scores and generate treatment assignment
   pi <- 0.2 * pnorm(0.5* gamma_x - 0.5 * x[,1])^2 + runif(n) / 10
-  z <- matrix(0, n, t1)
-  for (i in t0:t1){
+  z <- matrix(0, n, t2)
+  for (i in t0:t1){ # only assign treatment in observed period
     treated <- (z[, i - 1] == 1)
     treatment <- rbinom(n, 1, pi) # draw new treatment for time i
     z[, i] <- apply(cbind(treated, treatment), 1, max) 
+  }
+  if (t2 > t1){
+    z[,(t1 + 1): t2] <- matrix(rep(z[,t1], t2 - t1), n, t2 - t1)
   }
   results$z <- z
   
@@ -82,13 +91,121 @@ dgp <- function(n, t0 = 6, t1 = 12, pr_type = "non-parallel", trt_type = "hetero
   results$tau <- tau
   
   # generate outcome variable
-  results$y <-  y0 + tau + matrix(rnorm(n*t1, mean = 0, sd = 0.2), n, t1)
+  results$y <-  y0 + tau + matrix(rnorm(n*t2, mean = 0, sd = 0.2), n, t2)
+  
   
   return(results)
 }
 
+getCohort <- function(z){
+  # z: n by t treatment indicator at time t
+  # get cohorts that adopt the treatment at the same time
+  # 0 for control group
+  z.rowSums <- rowSums(z)
+  cohort <- max(z.rowSums) - z.rowSums + 1
+  cohort[z.rowSums == 0] <- 0
+  return(cohort)
+}
 
 # Calculate metrics -------------------------------------------------------
+
+getAttHat <- function(tauhat){
+  # tauhat: tauhats from longbet prediction, n by t by sweeps
+  # return: return estimated att across time
+  att.hat <- apply(tauhat, 2, mean)
+  return(att.hat)
+}
+
+getAttCI <- function(tauhat, alpha = 0.05){
+  # tauhat: tauhats from longbet prediction, n by t by sweeps
+  # return: return credible intervals across time
+  lower <- apply(tauhat, 2, quantile, probs = alpha/2)
+  upper <- apply(tauhat, 2, quantile, probs = 1 - alpha/2)
+  return(list(lower = lower, upper = upper))
+}
+
+getCohortAtt <- function(tau, cohort){
+  # return dataframe of ATT for each group at each time
+  tau.df <- data.frame(tau)
+  tau.df$cohort <- cohort
+  df <- tau.df %>% 
+    group_by(cohort) %>%
+    summarize(across(starts_with("X"), mean))
+  return(df)
+}
+
+getCohortAttHat <- function(tauhat, cohort){
+  # tauhat: tauhats from longbet prediction, n by t by sweeps
+  # cohort: a vector of length n indicating group for each unit
+  # return: dataframe of ATT for each group at each time
+  
+  unique.cohort <- sort(unique(cohort))
+  df <- data.frame(cohort = unique.cohort, matrix(NA, nrow = length(unique.cohort), ncol = dim(tauhat)[2]))
+  for (i in 1:length(unique.cohort)){
+    df[i,2:ncol(df)] <- getAttHat(tauhat[cohort == unique.cohort[i],,])
+  }
+  return(df)
+}
+
+getCohortAttCI <- function(tauhat, cohort, alpha = 0.05){
+  # tauhat: tauhats from longbet prediction, n by t by sweeps
+  # cohort: a vector of length n indicating group for each unit
+  # return: list of dataframe of credible intervals for each group at each time
+  
+  unique.cohort <- sort(unique(cohort))
+  lower <- data.frame(cohort = unique.cohort, matrix(NA, nrow = length(unique.cohort), ncol = dim(tauhat)[2]))
+  upper <- data.frame(cohort = unique.cohort, matrix(NA, nrow = length(unique.cohort), ncol = dim(tauhat)[2]))
+  for (i in 1:length(unique.cohort)){
+    CI <- getAttCI(tauhat[cohort == unique.cohort[i],,], alpha)
+    lower[i,2:ncol(lower)] <- CI$lower
+    upper[i,2:ncol(upper)] <- CI$upper
+  }
+  return(list(lower = lower, upper = upper))
+}
+
+getCohortMetrics <- function(att, att.hat, att.ci = NULL){
+  # att: ATT for each cohort, the first column is cohort
+  # att.hat: Estimated att for each cohort
+  # att.ci: A list of credible intervals of estimated att for each cohort
+  # return: A table of RMSE and Coverage (if provided) on estimated ATT for each cohort
+  
+  # check if cohort in att match the one in att.hat
+  if (names(att)[1] != 'cohort'){
+    print("The first column in att should be cohort")
+    stop()
+  }
+  if (names(att.hat)[1] != 'cohort'){
+    print("The first column in att.hat should be cohort")
+    stop()
+  }
+  if (any(att$cohort != att.hat$cohort)){
+    print("The cohort column in att.hat should match with att")
+    stop()
+  }
+  if (ncol(att) != ncol(att.hat)){
+    print("The dimension of att.hat does not match with att table")
+    stop()
+  }
+   
+  df <- data.frame(cohort = att$cohort, RMSE = NA, coverage = NA)
+  for (i in 1:nrow(df)){
+    df$RMSE[i] <- sqrt( mean( as.numeric( (att[i,-1] - att.hat[i, -1])^2 )) ) # get rmse
+  }
+  
+  if (is.null(att.ci)){
+    df$coverage <- NULL
+  } else {
+    if (any(names(att.ci) != c('lower', 'upper'))){
+      print("att.ci should have two tables named 'lower' and 'upper'.")
+      stop()
+    }
+    for (i in 1:nrow(df)){
+      df$coverage[i] <- mean( (att[i, 2:ncol(att)] >= att.ci$lower[i, 2:ncol(att)]) & (att[i, 2:ncol(att)] <= att.ci$upper[i, 2:ncol(att)]) )
+    }
+  }
+  return(df)
+}
+
 att.metric <- function(att, estimate){
   # check estimate has the right form
   if (!is.data.frame(estimate)){
